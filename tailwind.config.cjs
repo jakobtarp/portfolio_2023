a3beed9f06719611631861d94e8b5de192ec5b68
Jakob Tarp <jakob.tarp@outlook.com>
2023-03-23 16:16:00 +0100

    måske

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        transparent: "transparent",
+        brown: "#100D0B",
+        brown1: "#22160D",
+        brown2: "#311C0E",
+      },
+
       backgroundImage: {
         hero: "url('../public/img/baggrund.webp')",
         mobil: "url('../public/img/mobil.png')",
