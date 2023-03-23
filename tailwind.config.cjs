981cd6b379729a0c4b2fa5ac587f8cf4984962aa
Jakob Tarp <jakob.tarp@outlook.com>
2023-03-23 13:25:53 +0100

    hmm

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        hero: "url('../public/img/baggrund.webp')",
+        mobil: "url('../public/img/mobil.png')",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
