335172094470194c680386132b1e7c5bd1d68266
Jakob Tarp <jakob.tarp@outlook.com>
2023-03-24 15:54:12 +0100

    updated

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,16 +3,14 @@ module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        hero: "url('../public/img/noise.svg')",
+      },
       colors: {
         transparent: "transparent",
         brown: "#100D0B",
         brown1: "#22160D",
         brown2: "#311C0E",
-      },
-
-      backgroundImage: {
-        hero: "url('../public/img/baggrund.webp')",
-        mobil: "url('../public/img/mobil.png')",
       },
     },
   },
